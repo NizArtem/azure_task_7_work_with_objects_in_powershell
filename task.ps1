18b448e17aa130837ca81d56f68bc7ab5573db2f
Artem Nizamiiev <upuyayua@gmail.com>
2025-12-11 18:04:39 +0200

    Solved

--- task.ps1
+++ task.ps1
@@ -1,1 +1,15 @@
-# Write your code here
+$vmSize = "Standard_B2pts_v2"
+$Path = "./data/"
+$regions = @()
+
+
+foreach ($file in Get-ChildItem -Path $Path) {
+    $content = Get-Content $file.FullName -Raw
+
+    if ($content -match $vmSize) {
+        $regionName = [System.IO.Path]::GetFileNameWithoutExtension($file.Name)
+        $regions += $regionName
+    }
+}
+
+$regions | ConvertTo-Json | Set-Content "result.json"
